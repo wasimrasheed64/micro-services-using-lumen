ea8d9e42857f8227c7894e69d2d90436fece953c
= <=>
2024-09-11 12:29:02 +0500

    created factory and seeder for the factories

--- lumenAutherApis/database/seeders/DatabaseSeeder.php
+++ lumenAutherApis/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Author;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+//         new AuthorFactory()->
+        Author::factory()->count(2000)->create();
     }
 }
